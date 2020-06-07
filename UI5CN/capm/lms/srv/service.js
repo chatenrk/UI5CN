e64ffb1969a748c5a1b1c45bd40fcf64c8b65f16
Chaitanya Rayabharam <chaitanya.rayabharam@gmail.com>
2020-06-07 21:08:28 +0530

    Basic where condition

--- UI5CN/capm/lms/srv/service.js
+++ UI5CN/capm/lms/srv/service.js
@@ -3,7 +3,14 @@ const { Students } = cds.entities('myCompany.hr.lms');
 module.exports = (srv) => {
   srv.on('READ', 'Students', async (req, res) => {
     const { SELECT } = cds.ql(req);
-    const result = SELECT.from(Students);
+    const aFilter = req.query.SELECT.where;
+    if (typeof aFilter === 'undefined') {
+      return await SELECT.from(Students);
+    }
+
+    const result = await SELECT.from(Students).where({
+      email: aFilter[2].val,
+    });
     return result;
   });
 };
